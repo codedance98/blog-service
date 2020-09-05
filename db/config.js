f62492120636220a3a4c64aa8a1b7c2aa85f04e7
李良奇 <codedance98@gmail.com>
2020-09-05 17:02:11 +0800

    update aliyun config

--- db/config.js
+++ db/config.js
@@ -2,7 +2,7 @@ module.exports ={
     host: 'localhost',
     port: '3306',
     user: 'root',
-    password: '123456',
+    password: ';rr2v2Vye-Xl',
     database: 'myProject',
     timezone: '08:00'
 }
